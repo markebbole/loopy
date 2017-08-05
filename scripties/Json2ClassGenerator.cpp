43a2a276f0150a7b11d119dfe0ed38172e5f35c2
markebbole <markebbole@utexas.edu>
2017-08-05 14:22:33 -0700

    add inputs to gen script

--- scripties/Json2ClassGenerator.cpp
+++ scripties/Json2ClassGenerator.cpp
@@ -5,8 +5,16 @@ int main()
 {
     string c;
     while (cin >> c) {
-        cout << "if (type == \"" << c << "\") {\n"
+        if (c.at(c.length() - 1) == '_') {
+            c = c.substr(0, c.length()-1);
+            cout << "if (type == \"" << c << "\") {\n"
             << "    allNodes[k] = new " << c << "();\n"
+            << "    inputNodes.push_back((LoopyInputNode*)allNodes[k]);\n"
             << "}\n";
+        } else {
+            cout << "if (type == \"" << c << "\") {\n"
+                << "    allNodes[k] = new " << c << "();\n"
+                << "}\n";
+        }
     }
 }
