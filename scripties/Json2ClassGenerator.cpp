5704fbca1debbe489f67073c0b47f617af187a6a
markebbole <markebbole@utexas.edu>
2017-08-05 14:18:19 -0700

    remove commented out code

--- scripties/Json2ClassGenerator.cpp
+++ scripties/Json2ClassGenerator.cpp
@@ -3,12 +3,8 @@
 using namespace std;
 int main()
 {
-    //int numClasses;
-    //cin >> numClasses;
-    //for (int i = 0; i < numClasses; ++i) {
     string c;
     while (cin >> c) {
-        //cin >> c;
         cout << "if (type == \"" << c << "\") {\n"
             << "    allNodes[k] = new " << c << "();\n"
             << "}\n";
